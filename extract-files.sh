ef9cbe748d80f1a1a4ad234581b368d2b2f8ddc8
Nick Reuter <nreuter85@gmail.com>
2024-12-10 20:32:13 -0600

    sm8150-common: Address missing libbinder symbols in libwfdnative
    _ZN7android6binder12atrace_beginEmPKc
    _ZN7android6binder10atrace_endEm
    
    Change-Id: I942d51721461dbbf9c8d0f2f32128529bb7480c3

--- extract-files.sh
+++ extract-files.sh
@@ -72,6 +72,7 @@ function blob_fixup() {
     case "${1}" in
 	    system_ext/lib/libwfdnative.so|system_ext/lib64/libwfdnative.so)
             [ "$2" = "" ] && return 0
+            grep -q libbinder_shim.so "${2}" || "${PATCHELF}" --add-needed "libbinder_shim.so" "${2}"
             grep -q libinput_shim.so "${2}" || "${PATCHELF}" --add-needed "libinput_shim.so" "${2}"
             ;;
 	    system_ext/lib/libwfdmmsrc_system.so|system_ext/lib64/libwfdmmsrc_system.so)
