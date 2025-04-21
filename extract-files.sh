da4d21dc4e9d39f63d622caa589a3d7ce0cf2df5
Christian Hoffmann <chrmhoffmann@gmail.com>
2025-04-21 21:20:43 +0200

    sm8150-common: Drop 32bit libwfdnative
    Change-Id: I1053751d9a14300c2b423f9c83d9cdf4708edde2

--- extract-files.sh
+++ extract-files.sh
@@ -70,7 +70,7 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-	    system_ext/lib/libwfdnative.so|system_ext/lib64/libwfdnative.so)
+	    system_ext/lib64/libwfdnative.so)
             [ "$2" = "" ] && return 0
             grep -q libbinder_shim.so "${2}" || "${PATCHELF}" --add-needed "libbinder_shim.so" "${2}"
             grep -q libinput_shim.so "${2}" || "${PATCHELF}" --add-needed "libinput_shim.so" "${2}"
